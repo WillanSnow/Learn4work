c026979e8549abf0922a17413924a5c96e5bc102
Willansnow <chenp276@mail2.sysu.edu.cn>
2025-04-20 13:45:43 +0800

    0420_hide_sth_private

--- Code/CCF/2023_12/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    long long a = 1e10;
-    cout<<a;
-}
--- Code/CCPC2023/Online/A.py
+++
@@ -1,5 +0,0 @@
-ret = 0
-for i in range(0,5):
-    inp = input().replace(' ','').lower()
-    ret += inp.count("ass")
-print(ret)
